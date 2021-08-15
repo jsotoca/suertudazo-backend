68fed7dbf82728c0849b598810d543ebc8606a9d
jsotoca <jsotoca@unprg.edu.pe>
2021-08-15 14:46:03 -0500

    Se configuro cron para los miercoles y domingos a las 10:45 pm

--- utils/cron.js
+++ utils/cron.js
@@ -3,10 +3,11 @@ const { scrappingResultadosTinka } = require('./scrapping');
 const db = require('./db');
 
 function tarea_programada(){
-    cron.schedule('* * * * *',async ()=>{
+    cron.schedule('45 22 * * 3,7',async ()=>{
         const resultados = await scrappingResultadosTinka();
+        const fecha = new Date();
         db.setItem('resultados',JSON.stringify(resultados));
-        console.log('Se ejecuto tarea programada.');
+        console.log(`Se ejecuto tarea programada ${fecha}`);
     });
 }
 
