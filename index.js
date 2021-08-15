644db667a096e26e89a31bbab452d6a80b524403
jsotoca <jsotoca@unprg.edu.pe>
2021-08-15 15:12:15 -0500

    Se agrego cors helmet y compression a express

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
 const express               = require('express');
+const cors                  = require('cors');
+const helmet                = require('helmet');
+const compression           = require('compression');
 const db                    = require('./utils/db');
 const { tarea_programada }  = require('./utils/cron');
 const { APP_NAME, PORT }    = require('./utils/config');
@@ -6,6 +9,9 @@ const { APP_NAME, PORT }    = require('./utils/config');
 const server = express();
 tarea_programada();
 
+server.use(cors());
+server.use(helmet());
+server.use(compression());
 server.use('/',(req,res) => {
     const resultados = JSON.parse(db.getItem('resultados'));
     res.json({
